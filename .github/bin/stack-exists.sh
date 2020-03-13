8dde7a0d4163bcbda279217b7ee570d8aea46c98
Feng Li <fen9li@outlook.com>
2020-03-13 15:42:09 +1100

    add aws lambda function cloudformation template skeleton

---
+++ .github/bin/stack-exists.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# check if cloudformation stack exists
+
+if ! aws cloudformation describe-stacks --region ${AWS_REGION} --stack-name "${stackName}"; then
+    echo 'stack doesnt exist, creating ...' 
+    aws cloudformation create-stack --stack-name "${stackName}" --template-url "${stackTemplateUrl}" --capabilities CAPABILITY_IAM --parameters ParameterKey=env,ParameterValue="${env}" 
+else
+    echo 'stack exists, updating ...'
+    aws cloudformation update-stack --stack-name "${stackName}" --template-url "${stackTemplateUrl}" --capabilities CAPABILITY_IAM --parameters ParameterKey=env,ParameterValue="${env}"
+fi
+
+exit 0
