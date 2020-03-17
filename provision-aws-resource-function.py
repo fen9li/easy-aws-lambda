c5ee31b318c8199dbda050d3d8611a31dfe3c868
Feng Li <fen9li@outlook.com>
2020-03-17 21:39:19 +1100

    lambda function python code to list s3 bucket object keys by using boto3 add s3 full access to lambda function

--- provision-aws-resource-function.py
+++ provision-aws-resource-function.py
@@ -1,8 +1,10 @@
+import boto3
 import json
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+
+    s3 = boto3.resource('s3')
+    my_bucket = s3.Bucket('lambda-function-stack-templates-home')
+
+    for obj in my_bucket.objects.all():
+        print(obj.key)
