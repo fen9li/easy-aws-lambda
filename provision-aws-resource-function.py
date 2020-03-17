3fe3837e3259c1af6a148ce9d32ace1f4308a225
Feng Li <fen9li@outlook.com>
2020-03-17 21:28:10 +1100

    easy lambda function template - python

--- provision-aws-resource-function.py
+++ provision-aws-resource-function.py
@@ -1,59 +1,8 @@
- stack_name = 'simple-sinatra'
-    templateDocument = 'templates/sinatra/simple-sinatra-cftemplate-dev.yml'
-    parameters = 'parameters/parameters.json'
+import json
 
-    template_data = _parse_template(templateDocument)
-    parameter_data = _parse_parameters(parameters)
-
-    params = {
-        'StackName': stack_name,
-        'TemplateBody': template_data,
-        'Parameters': parameter_data,
-    }
-
-    try:
-        if _stack_exists(stack_name):
-            pass
-        else:
-            print('Creating {}'.format(stack_name))
-            stack_result = cf.create_stack(**params)
-            waiter = cf.get_waiter('stack_create_complete')
-        print("...waiting for stack to be ready...")
-        waiter.wait(StackName=stack_name)
-    except botocore.exceptions.ClientError as ex:
-        error_message = ex.response['Error']['Message']
-        if error_message == 'No updates are to be performed.':
-            print("No changes")
-        else:
-            raise
-    else:
-        print(json.dumps(
-            cf.describe_stacks(StackName=stack_result['StackId']),
-            indent=2,
-            default=json_serial
-        ))
-    
+def lambda_handler(event, context):
+    # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Lambda function finished successfully!')
+        'body': json.dumps('Hello from Lambda!')
     }
-
-def _parse_template(template):
-    with open(template) as template_fileobj:
-        template_data = template_fileobj.read()
-    cf.validate_template(TemplateBody=template_data)
-    return template_data
-
-def _parse_parameters(parameters):
-    with open(parameters) as parameter_fileobj:
-        parameter_data = json.load(parameter_fileobj)
-    return parameter_data
-
-def _stack_exists(stack_name):
-    stacks = cf.list_stacks()['StackSummaries']
-    for stack in stacks:
-        if stack['StackStatus'] == 'DELETE_COMPLETE':
-            continue
-        if stack_name == stack['StackName']:
-            return True
-    return False
